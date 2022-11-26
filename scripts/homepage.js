240812a7f59e578f66a8822c698ca9141c403f23
sopankawade97 <kawade.sopan2015@gmail.com>
2022-11-26 23:54:38 +0530

    working on data

---
+++ scripts/homepage.js
@@ -0,0 +1,55 @@
+const slider = [
+  {
+    image: "",
+    text1: "",
+    text2: "",
+    button: "",
+  },
+  {
+    image: "",
+    text1: "",
+    text2: "",
+    button: "",
+  },
+  {
+    image: "",
+    text1: "",
+    text2: "",
+    button: "",
+  },
+  {
+    image: "",
+    text1: "",
+    text2: "",
+    button: "",
+  },
+];
+
+const threeImages = [
+  {
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0248/3473/6191/files/asapimage---533-x-644-px_540x.jpg?v=1650550832",
+    info: "DRESSE FOR EVERY OCCASION",
+    discount: "UPTO 94% OFF",
+    extraInfo: "Featuring BCBGMaxazria,<br> Halston, Kay Unger & More",
+    btn: "SHOP DRESSES",
+  },
+  {
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0248/3473/6191/files/WEB_ASSET_2_540x.jpg?v=1650894933",
+    info: "DESIGNER BOUTIQUE",
+    discount: "UPTO 84% OF",
+    extraInfo:
+      "Best-Selling Styles from: Gucci,<br> Valentino, Balenciaga, Stuart Weitzman & More",
+    btn: "SHOP DESIGNER BOTIQUE",
+  },
+  {
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0248/3473/6191/files/womens_tops_2_540x.jpg?v=1630508297",
+    info: "TRENDING TOPS FOR THE NEW SEASON",
+    discount: "",
+    extraInfo: "Featuring Gracia, Theory, Trina<BR> Turk & More",
+    btn: "SHOP DRESSES",
+  },
+];
+
